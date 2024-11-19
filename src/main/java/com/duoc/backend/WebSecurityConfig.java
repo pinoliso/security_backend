ffd341d086f34073051c0fa3fe97c9dd17d145fe
Enrique Pino <pinoliso@gmail.com>
2024-11-18 22:12:59 -0300

    sonarqube check

--- src/main/java/com/duoc/backend/WebSecurityConfig.java
+++ src/main/java/com/duoc/backend/WebSecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @EnableWebSecurity()
@@ -21,13 +25,38 @@ class WebSecurityConfig{
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http
+                // .cors(cors -> corsConfigurationSource())
                 .csrf((csrf) -> csrf
                         .disable())
-                .authorizeHttpRequests( authz -> authz
-                        .requestMatchers(HttpMethod.POST,Constants.LOGIN_URL).permitAll()
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, Constants.LOGIN_URL).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/recipes").permitAll()
                         .anyRequest().authenticated())
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // Permitir todos los orígenes (puedes especificar orígenes específicos)
+        config.addAllowedHeader("*"); // Permitir todos los encabezados
+        config.addAllowedMethod("*"); // Permitir todos los métodos (GET, POST, etc.)
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.addAllowedOriginPattern("*"); // Configura los orígenes permitidos
+        configuration.addAllowedMethod("*"); // Permite todos los métodos HTTP
+        configuration.addAllowedHeader("*"); // Permite todos los encabezados
+        configuration.setAllowCredentials(true); // Permite las credenciales
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/duoc/backend/controller/RecipeController.java
+++ src/main/java/com/duoc/backend/controller/RecipeController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,4 +38,29 @@ public class RecipeController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
+        return recipeService.saveRecipe(recipe);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Recipe> updateRecipe(@PathVariable Long id, @RequestBody Recipe recipe) {
+        Optional<Recipe> oldRecipe = recipeService.getRecipeById(id);
+        if (oldRecipe.isPresent()) {
+            recipe.setId(id);
+            return ResponseEntity.ok(recipeService.saveRecipe(recipe));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecipe(@PathVariable Long id) {
+        recipeService.deleteRecipe(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }
